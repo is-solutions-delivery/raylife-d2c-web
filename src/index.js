7b1d02914afd2d3aa51b1a66fcd143021023cf38
Giuseppe Davanzo <giuseppe.davanzo@objective.com.br>
2021-06-29 11:21:31 -0300

    adapt to work with web components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { StyleSheetManager } from "styled-components";
 
 import { App } from "./App";
 import { Providers } from "./Providers";
 
-ReactDOM.render(
-  <Providers>
-    <App />
-  </Providers>,
-  document.getElementById("root")
-);
+class WebComponent extends HTMLElement {
+  constructor() {
+    super();
+    this.styleHost = document.createElement("div");
+    this.mountPoint = document.createElement("div");
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    this.shadowRoot.appendChild(this.styleHost);
+    this.shadowRoot.appendChild(this.mountPoint);
+
+    ReactDOM.render(
+      <StyleSheetManager target={this.styleHost}>
+        <Providers>
+          <App />
+        </Providers>
+      </StyleSheetManager>,
+      this.mountPoint
+    );
+  }
+}
+
+if (!customElements.get("d2c-web")) {
+  customElements.define("d2c-web", WebComponent);
+}
