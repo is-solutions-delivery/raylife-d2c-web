1d70fa7eb9e58a2cd2671ba371a8e1cafac5a410
Giuseppe Davanzo <giuseppe.davanzo@objective.com.br>
2021-07-30 15:50:15 -0300

    Abstract InputWithMask components

---
+++ src/components/fragments/Forms/Input/WithMask/Phone.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { InputWithMask } from ".";
+
+export const InputWithPhoneMask = React.forwardRef((props, ref) => {
+  return (
+    <InputWithMask
+      {...props}
+      ref={ref}
+      format="(###) ###-####"
+      label="Phone"
+      mask="_"
+    />
+  );
+});
